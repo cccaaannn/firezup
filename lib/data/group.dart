33066b3b13c4c24ad60d3dfe7958680faf344873
cccaaannn <can.kurt.aa@gmail.com>
2023-05-19 23:03:24 +0300

    owner added

--- lib/data/group.dart
+++ lib/data/group.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Group {
-  Group(this.id, this.name, this.timestamp, this.members);
+  Group(this.id, this.name, this.timestamp, this.members, this.owner);
 
   String id;
   String name;
   Timestamp timestamp;
   List<String> members;
+  String owner;
 }
--- lib/data/group_search.dart
+++ lib/data/group_search.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GroupSearch {
-  GroupSearch(this.id, this.name, this.timestamp, this.joined);
+  GroupSearch(this.id, this.name, this.timestamp, this.joined, this.owner);
 
   String id;
   String name;
   Timestamp timestamp;
   bool joined;
+  String owner;
 }
